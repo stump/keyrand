1e657f74789d4e026f718609fadb1eeb74a91fd7
stump <stump@stump.io>
2018-10-25 23:37:14 -0700

    Validate that the bytes we patch are the ones we think they are.

--- keyrand.py
+++ keyrand.py
@@ -264,8 +264,12 @@ class KeyItemRandomizer(object):
         # using slice assignment to apply the patches if what was passed is a bytearray.
         rom = memoryview(rom)
 
-        def replace(rom, addr, newhex):
+        def replace(rom, addr, oldhex, newhex):
+            oldbin = binascii.unhexlify(oldhex)
             newbin = binascii.unhexlify(newhex)
+            assert len(oldbin) == len(newbin)
+            if rom[addr:addr+len(oldbin)] != oldbin:
+                raise UnrecognizedROM(addr)
             rom[addr:addr+len(newbin)] = newbin
 
         def replace_item(rom, addr, name):
@@ -275,15 +279,15 @@ class KeyItemRandomizer(object):
 
         # Bicycle slot
         #At 0x1d754, fc (jump fixup)
-        replace(rom, 0x1d754, 'fc')
+        replace(rom, 0x1d754, 'f5', 'fc')
         #At 0x1d75c, 2c (jump fixup)
-        replace(rom, 0x1d75c, '2c')
+        replace(rom, 0x1d75c, '2f', '2c')
         #At 0x1d765, new item
         replace_item(rom, 0x1d765, 'BICYCLE')
         #At 0x1d783, 78 (jump fixup)
-        replace(rom, 0x1d783, '78')
+        replace(rom, 0x1d783, '71', '78')
         #At 0x1d787, 18f6211058cd493cafea26ccea2acc3e03ea29cc3e01ea28ccea25cc3cea24cc2130d7cbf621a0c3010f04cd2219cd29243e06ea1ed1cdcf2f21cac3116dcdcd551911ff57cd551921e4c3110758cd5519211558cd493ccdbe3a2130d7cbb6cb4f200cfa26cca72006211a58cd493c212a58cd493cc3d724 (put correct item in menu when you don't have voucher, and patch instant text)
-        replace(rom, 0x1d787, '18f6211058cd493cafea26ccea2acc3e03ea29cc3e01ea28ccea25cc3cea24cc2130d7cbf621a0c3010f04cd2219cd29243e06ea1ed1cdcf2f21cac3116dcdcd551911ff57cd551921e4c3110758cd5519211558cd493ccdbe3a2130d7cbb6cb4f200cfa26cca72006211a58cd493c212a58cd493cc3d724')
+        replace(rom, 0x1d787, 'cd493c1869211058cd493cafea26ccea2acc3e03ea29cc3e01ea28cc3e02ea24cc3e01ea25cc2130d7cbf621a0c306040e0fcd2219cd292421cac311f857cd551921e4c3110758cd5519211558cd493ccdbe3acb4f20112130d7cbb6fa26cca72006211a58cd493c212a58cd493cc3d72481888298828b84', '18f6211058cd493cafea26ccea2acc3e03ea29cc3e01ea28ccea25cc3cea24cc2130d7cbf621a0c3010f04cd2219cd29243e06ea1ed1cdcf2f21cac3116dcdcd551911ff57cd551921e4c3110758cd5519211558cd493ccdbe3a2130d7cbb6cb4f200cfa26cca72006211a58cd493c212a58cd493cc3d724')
         #At 0x1d78b, 70 (jump fixup)
         #rom[0x1d78b] = 0x70
         #At 0x1d7a3, ea25cc3cea24cc2130d7cbf621a0c3010f04cd2219cd29243e46cdcf2f21cac3116dcdcd551911ff57cd551921e4c3110758cd5519211558cd493ccdbe3acb4f20112130d7cbb6fa26cca72006211a58cd493c212a58cd493cc3d724 (put correct item in menu when you don't have voucher)
@@ -291,15 +295,15 @@ class KeyItemRandomizer(object):
         #At 0x1d7b9, new item
         replace_item(rom, 0x1d7b9, 'BICYCLE')
         #At 0x98ed4, 50014bcf00e8505000 (so text from wcf4b is used)
-        replace(rom, 0x98ed4, '50014bcf00e8505000')
+        replace(rom, 0x98ed4, 'a07f81888298828b84', '50014bcf00e8505000')
 
         # Bike Voucher slot
         #At 0x59b78, c9 (give item even if bike or voucher already in bag)
-        replace(rom, 0x59b78, 'c9')
+        replace(rom, 0x59b78, 'c0', 'c9')
         #At 0x59c39, new item
         replace_item(rom, 0x59c39, 'BIKE VOUCHER')
         #At 0x9a83a, 50014bcf00e7505000 (remove "a" before item name in text)
-        replace(rom, 0x9a83a, '50014bcf00e7505000')
+        replace(rom, 0x9a83a, 'a07f50014bcf00e750', '50014bcf00e7505000')
 
         # Card Key slot
         #At 0x1a0e7, new item
@@ -309,22 +313,22 @@ class KeyItemRandomizer(object):
         #At 0x49183, new item
         replace_item(rom, 0x49183, 'COIN CASE')
         #At 0x9e086, 50014bcf00e7505000 (remove "a" before item name in text)
-        replace(rom, 0x9e086, '50014bcf00e7505000')
+        replace(rom, 0x9e086, 'a07f50014bcf00e750', '50014bcf00e7505000')
 
         # Dome Fossil slot
         #At 0x49e40, 24 (pointer to adjusted text)
-        replace(rom, 0x49e40, '24')
+        replace(rom, 0x49e40, '29', '24')
         #At 0x49eef, 3e29cdcf2f211f5fcd493ccdec35fa26cca7205e010129cd2e3ed2765fcd695f21f6d7cbf63e6dc3515f000000000000175e492050083e01ea3ccc3e2acdcf2f (put correct item in text when confirming picking up items)
         #rom[0x49eef:0x49f2f] = binascii.unhexlify('3e29cdcf2f211f5fcd493ccdec35fa26cca7205e010129cd2e3ed2765fcd695f21f6d7cbf63e6dc3515f000000000000175e492050083e01ea3ccc3e2acdcf2f')
-        replace(rom, 0x49eef, '3e29cd195f211f5fcd493ccdec35fa26cca7205e010129cd2e3ed2765fcd695f21f6d7cbf63e6dc3515fea1ed1c3cf2f175e492050083e01ea3ccc3e2acd195f')
+        replace(rom, 0x49eef, '21245fcd493ccdec35fa26cca72023010129cd2e3ed2765fcd695f3e6dea4dcc3e11cd6d3e21f6d7cbf63e04ea07d6ea39dac3d724175e492050083e01ea3ccc', '3e29cd195f211f5fcd493ccdec35fa26cca7205e010129cd2e3ed2765fcd695f21f6d7cbf63e6dc3515fea1ed1c3cf2f175e492050083e01ea3ccc3e2acd195f')
         #At 0x49ef0, new item
         replace_item(rom, 0x49ef0, 'DOME FOSSIL')
         #At 0x49f05, new item
         replace_item(rom, 0x49f05, 'DOME FOSSIL')
         #At 0x49f4a, 21f6d7cbfe3e6eea4dcc3e11cd6d3e (put correct item in text when confirming picking up items)
-        replace(rom, 0x49f4a, '21f6d7cbfe3e6eea4dcc3e11cd6d3e')
+        replace(rom, 0x49f4a, '3e6eea4dcc3e11cd6d3e21f6d7cbfe', '21f6d7cbfe3e6eea4dcc3e11cd6d3e')
         #At 0x8096c, 50016dcd00e65700 (remove "the" before item name in text)
-        replace(rom, 0x8096c, '50016dcd00e65700')
+        replace(rom, 0x8096c, '838e8c847f858e92', '50016dcd00e65700')
         #At 0x8099b, 4f50014bcf00e7505000 (remove "the" before item name in text)
         #rom[0x8099b:0x809a5] = binascii.unhexlify('4f50014bcf00e7505000')
 
@@ -336,7 +340,7 @@ class KeyItemRandomizer(object):
         #At 0x5619a, new item
         replace_item(rom, 0x5619a, 'GOOD ROD')
         #At 0xa072d, 50014bcf00e7505000 (remove "a" before item name in text)
-        replace(rom, 0xa072d, '50014bcf00e7505000')
+        replace(rom, 0xa072d, 'a07f50014bcf00e750', '50014bcf00e7505000')
 
         # Helix Fossil slot
         # Fixups from Dome Fossil
@@ -345,7 +349,7 @@ class KeyItemRandomizer(object):
         #At 0x49f40, new item
         replace_item(rom, 0x49f40, 'HELIX FOSSIL')
         #At 0x80987, 50016dcd00e65700 (remove "the" before item name in text)
-        replace(rom, 0x80987, '50016dcd00e65700')
+        replace(rom, 0x80987, '87848b88977f858e', '50016dcd00e65700')
 
         # HM01 slot
         #At 0x618c3, new item
@@ -357,7 +361,7 @@ class KeyItemRandomizer(object):
         #At 0x1e637, 7c (pointer to adjusted text after the HM02 text)
         #At 0x8ce72, 50014bcf00e75050000000878cf6f87fa8b27f858b98e84f88b3bc (so text from wcf4b is used; adjust first text afterward to not have to shift everything after in way that minimizes length of this patch)
         #Instead of those two, at 0x1e631, 9954 (repoint text to S.S. Ticket text, which uses text from wcf4b)
-        replace(rom, 0x1e631, '9954')
+        replace(rom, 0x1e631, '664e', '9954')
 
         # HM03 slot
         #At 0x4a32c, new item
@@ -369,11 +373,11 @@ class KeyItemRandomizer(object):
 
         # HM05 slot
         #At 0x540f3, 6d (remove Cut tree in the way)
-        replace(rom, 0x540f3, '6d')
+        replace(rom, 0x540f3, '32', '6d')
         #At 0x5d5e8, new item
         replace_item(rom, 0x5d5e8, 'HM05')
         #At 0x801a3, 5550015bcc00e7500505 (remove "an" before item name in aide script text)
-        replace(rom, 0x801a3, '5550015bcc00e7500500')
+        replace(rom, 0x801a3, '7fa0ad5550015bcc00e7', '5550015bcc00e7500500')
         #At 0x80244, 5550015bcc00e85700 (remove "the" before item name in aide script text)
         #rom[0x80244:0x8024d] = binascii.unhexlify('5550015bcc00e85700')
         #At 0x802df, 4f50015bcc00e7505000 (remove "the" before item name in aide script text)
@@ -394,39 +398,39 @@ class KeyItemRandomizer(object):
 
         # Oak's Parcel slot
         #At 0x1d46e, 000000 (delay shuffling of mart text pointers)
-        replace(rom, 0x1d46e, '000000')
+        replace(rom, 0x1d46e, 'cd7d54', '000000')
         #At 0x1d499, 7d (delay shuffling of mart text pointers)
-        replace(rom, 0x1d499, '7d')
+        replace(rom, 0x1d499, 'df', '7d')
         #At 0x1d4c8, 010146cd2e3e3e05e08ccd2029 (so right text is in wcf4b)
-        replace(rom, 0x1d4c8, '010146cd2e3e3e05e08ccd2029')
+        replace(rom, 0x1d4c8, '3e05e08ccd2029010146cd2e3e', '010146cd2e3e3e05e08ccd2029')
         #At 0x1d4ca, new item
         replace_item(rom, 0x1d4ca, 'OAK\'s PARCEL')
         #At 0x95cc7, 50014bcf00e7505000 (so text from wcf4b is used)
-        replace(rom, 0x95cc7, '50014bcf00e7505000')
+        replace(rom, 0x95cc7, '8e808abd7f8f809182', '50014bcf00e7505000')
 
         # Old Amber slot
         #At 0x5c266, new item
         replace_item(rom, 0x5c266, 'OLD AMBER')
         #At 0x9679c, 50014bcf00e7505000 (so text from wcf4b is used)
-        replace(rom, 0x9679c, '50014bcf00e7505000')
+        replace(rom, 0x9679c, '8e8b837f808c818491', '50014bcf00e7505000')
 
         # Old Rod slot
         #At 0x5608e, new item
         replace_item(rom, 0x5608e, 'OLD ROD')
         #At 0x9c599, 50014bcf00e7505000 (remove "an" before item name in text)
-        replace(rom, 0x9c599, '50014bcf00e7505000')
+        replace(rom, 0x9c599, 'a0ad7f50014bcf00e7', '50014bcf00e7505000')
 
         # Poké Flute slot
         #At 0x1d928, new item
         replace_item(rom, 0x1d928, 'POKé FLUTE')
         #At 0x9a007, 50014bcf00e7505000 (remove "a" before item name in text)
-        replace(rom, 0x9a007, '50014bcf00e7505000')
+        replace(rom, 0x9a007, 'a07f50014bcf00e750', '50014bcf00e7505000')
 
         # S.S. Ticket slot
         #At 0x1e884, new item
         replace_item(rom, 0x1e884, 'S.S. TICKET')
         #At 0x8d4a5, 50014bcf00e7505000 (remove "an" before item name in text)
-        replace(rom, 0x8d4a5, '50014bcf00e7505000')
+        replace(rom, 0x8d4a5, 'a0ad7f50014bcf00e7', '50014bcf00e7505000')
 
         # Secret Key slot
         #At 0x524d8, new item
@@ -440,23 +444,23 @@ class KeyItemRandomizer(object):
         #At 0x5649d, new item
         replace_item(rom, 0x5649d, 'SUPER ROD')
         #At 0x8ca45, 50014bcf00e7505000 (remove "a" before item name in text)
-        replace(rom, 0x8ca45, '50014bcf00e7505000')
+        replace(rom, 0x8ca45, 'a07f50014bcf00e750', '50014bcf00e7505000')
 
         # Town Map slot
         #At 0x19b69, 5f (give you the item after just beating Rival I, not after delivering parcel)
-        replace(rom, 0x19b69, '5f')
+        replace(rom, 0x19b69, '6f', '5f')
         #At 0x19b7c, new item
         replace_item(rom, 0x19b7c, 'TOWN MAP')
         #At 0x94ca2, 4f50014bcf00e7505000 (remove "a" before item name in text)
-        replace(rom, 0x94ca2, '4f50014bcf00e7505000')
+        replace(rom, 0x94ca2, '7fa04f50014bcf00e750', '4f50014bcf00e7505000')
 
         # Add randomizer info to the main menu screen.
         #At 0x05b20, 497c (reroute the menu's clear-screen call to new code)
-        replace(rom, 0x05b20, '497c')
+        replace(rom, 0x05b20, '0f19', '497c')
         #At 0x05ba8, 257d (reroute the menu's options screen to new code)
-        replace(rom, 0x05ba8, '257d')
+        replace(rom, 0x05ba8, '8a5e', '257d')
         #At 0x07c49, cd0f193effea2fd43e03ea67d33e19cd6d3ecd6100cde809cd7b0021b0c311bd7ccd55192192c411c27ccd55192154c411d37ccd55192168c411e77ccd551921bec411fb7ccd551921d4c411057dcd551921e6c4110f7dcd551921fcc4111a7dcd551921cec43e02223c7721e2c43e12223c77c9b5f6e8f85092a4a4a39c7ffaf8fffafffcfdf8fffb50b2b3b4acafbd7f86a4ada4b1a0b3a8aead7ff7508aa4b87f88b3a4ac7f91a0ada3aeaca8b9a4b150b2b3b4acafe8a8aef350a8b3a4acb1a0ada3ae50b3b6a8b3a2a7e8b3b5f350b2b3b4acafa3aeb3a8ae50cd0f19c38a5e (new code)
-        replace(rom, 0x07c49, 'cd0f193effea2fd43e03ea67d33e19cd6d3ecd6100cde809cd7b0021b0c311bd7ccd55192192c411c27ccd55192154c411d37ccd55192168c411e77ccd551921bec411fb7ccd551921d4c411057dcd551921e6c4110f7dcd551921fcc4111a7dcd551921cec43e02223c7721e2c43e12223c77c9b5f6e8f85092a4a4a39c7ffaf8fffafffcfdf8fffb50b2b3b4acafbd7f86a4ada4b1a0b3a8aead7ff7508aa4b87f88b3a4ac7f91a0ada3aeaca8b9a4b150b2b3b4acafe8a8aef350a8b3a4acb1a0ada3ae50b3b6a8b3a2a7e8b3b5f350b2b3b4acafa3aeb3a8ae50cd0f19c38a5e')
+        replace(rom, 0x07c49, '00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000', 'cd0f193effea2fd43e03ea67d33e19cd6d3ecd6100cde809cd7b0021b0c311bd7ccd55192192c411c27ccd55192154c411d37ccd55192168c411e77ccd551921bec411fb7ccd551921d4c411057dcd551921e6c4110f7dcd551921fcc4111a7dcd551921cec43e02223c7721e2c43e12223c77c9b5f6e8f85092a4a4a39c7ffaf8fffafffcfdf8fffb50b2b3b4acafbd7f86a4ada4b1a0b3a8aead7ff7508aa4b87f88b3a4ac7f91a0ada3aeaca8b9a4b150b2b3b4acafe8a8aef350a8b3a4acb1a0ada3ae50b3b6a8b3a2a7e8b3b5f350b2b3b4acafa3aeb3a8ae50cd0f19c38a5e')
 
         # Patch the seed into the new main menu screen info.
         seedbytes = str(self.seed).rjust(10).translate({
